e918d6f816656581a524b61f7663904c2424308c
Mohammad El-Tawil <mohammadeltawil0@gmail.com>
2026-02-07 00:00:04 -0500

    Implemented ReportDto

--- rusafe-backend/src/main/java/edu/rutgers/rusafe/dto/ReportDto.java
+++ rusafe-backend/src/main/java/edu/rutgers/rusafe/dto/ReportDto.java
@@ -1,4 +1,30 @@
 package edu.rutgers.rusafe.dto;
 
+import edu.rutgers.rusafe.model.enums.ReportType;
+import edu.rutgers.rusafe.model.enums.StatusType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.awt.*;
+import java.sql.Time;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReportDto {
+
+    private Long id;
+    private String title;
+    private String caption;
+    private Time timeCreated;
+    private ReportType reportType;
+    private StatusType status;
+    private Boolean anonymous;
+    private String imageURL;
+    private Double latitude;
+    private Double longitude;
 }
